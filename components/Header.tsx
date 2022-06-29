c1be6ee916b3155c7405c2fe3e2d377663c4883f
vishalpoonacha <vishalpoonacha007@gmail.com>
2022-06-29 20:10:15 +0530

    postid page rendering

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,18 +17,21 @@ import {
   VideoCameraIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 function Header() {
   const { data: session } = useSession();
 
   return (
-    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+    <div className="sticky items-center top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
-        <Image
-          objectFit="contain"
-          src="https://links.papareact.com/fqy"
-          layout="fill"
-        />
+        <Link href="/">
+          <Image
+            objectFit="contain"
+            src="https://links.papareact.com/fqy"
+            layout="fill"
+          />
+        </Link>
       </div>
       <div className="flex items-center mx-7 xl:min-w-[300px]">
         <HomeIcon className="h-5 w-5" />
